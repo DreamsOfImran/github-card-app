69e6bc768c06f210c25e75ec01b0bf0495d0b3e3
DreamsOfImran <ibasha66@gmail.com>
2020-04-05 00:36:06 +0530

    Simple github-card-app using react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  // GitHub usernames: gaearon, sophiebits, sebmarkbage, bvaughn
+
+  const CardList = (props) => (
+    <div>
+      {props.profiles.map((profile) => (
+        <Card key={profile.id} profile={profile} />
+      ))}
     </div>
   );
-}
+
+  const Card = ({ profile }) => (
+    <div className="github-profile">
+      <img src={profile.avatar_url} alt={profile.name} />
+      <div className="info">
+        <div className="name">{profile.name}</div>
+        <div className="company">{profile.company}</div>
+      </div>
+    </div>
+  );
+
+  const Form = (props) => {
+    const [userName, setUserName] = useState("");
+
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      const resp = await axios.get(`https://api.github.com/users/${userName}`);
+      props.onSubmit(resp.data);
+      setUserName("");
+    };
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={userName}
+          onChange={(event) => setUserName(event.target.value)}
+          placeholder="GitHub username"
+          required
+        />
+        <button>Add card</button>
+      </form>
+    );
+  };
+
+  const App = ({ title }) => {
+    const [profiles, setProfiles] = useState([]);
+
+    const addNewProfile = (profileData) => {
+      setProfiles([...profiles, profileData]);
+    };
+    return (
+      <div>
+        <div className="header">{title}</div>
+        <Form onSubmit={addNewProfile} />
+        <CardList profiles={profiles} />
+      </div>
+    );
+  };
+
+  return <App title="GitHub Card App" />;
+};
 
 export default App;
