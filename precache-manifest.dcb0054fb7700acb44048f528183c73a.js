e57eec993ed37215787238249fb102f439bad89f
DreamsOfImran <ibasha66@gmail.com>
2021-08-15 06:36:27 +0000

    Updates

---
+++ precache-manifest.dcb0054fb7700acb44048f528183c73a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a0b46d58a2f15387de153c513952f2",
+    "url": "/github-card-app/index.html"
+  },
+  {
+    "revision": "e515e854ab5f51366bd1",
+    "url": "/github-card-app/static/css/main.27036410.chunk.css"
+  },
+  {
+    "revision": "833b235c03ac63e322e9",
+    "url": "/github-card-app/static/js/2.86d5d10a.chunk.js"
+  },
+  {
+    "revision": "0fce063a4ec3cb005e7265693b407036",
+    "url": "/github-card-app/static/js/2.86d5d10a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e515e854ab5f51366bd1",
+    "url": "/github-card-app/static/js/main.d26017f6.chunk.js"
+  },
+  {
+    "revision": "17472f703e9ca0bf9090",
+    "url": "/github-card-app/static/js/runtime-main.fdcffd00.js"
+  }
+]);
